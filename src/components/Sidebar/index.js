132d49056fe6ac3a7403960d3424f6aed94874c8
Maksim Iliukovich <admin@blweb.ru>
2020-04-11 22:13:07 +0300

    review fix

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './Sidebar.css';
 import {useStore} from 'effector-react'
-import {$noRoute, $route} from "../../models/route/state";
+import {$noRoute} from "../../models/route/state";
 import BuildForm from "../BuildForm";
 import Route from "../Route";
 
 const Sidebar = () => {
-    const {route} = useStore($route);
     const noRoute = useStore($noRoute);
 
     return (
         <aside className="App-sidebar">
             <div className="App-sidebar__container">
-                {noRoute && <BuildForm/>}
-                {route && <Route/>}
+                {noRoute ? <BuildForm/> : <Route/>}
                 <div className="App-sidebar__footer-block">
                     Download on <a href="https://play.google.com/store/apps/details?id=ru.travelpath">Google Play</a>
                 </div>
