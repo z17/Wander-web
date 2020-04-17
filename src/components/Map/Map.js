95c1a7d0d98da9503c792d10ec62b02cba7ba9a5
YanLobat <yanlavryushev@gmail.com>
2020-04-18 00:59:41 +0300

    End up with the dataflow in map and points

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createGate, useStore} from 'effector-react'
+import {useStore} from 'effector-react'
 
 import './Map.css';
 
--- src/models/map/index.js
+++ src/models/map/index.js
@@ -22,6 +22,8 @@ export const removeRouteFromMapFx = createEffect();
 export const selectPositionEvent = createEvent();
 export const mapPositionUpdatedEvent = createEvent();
 
+export const MapGate = createGate();
+
 export const default_lat = 59.9402;
 export const default_lon = 30.3154;
 export const default_zoom = 12;
